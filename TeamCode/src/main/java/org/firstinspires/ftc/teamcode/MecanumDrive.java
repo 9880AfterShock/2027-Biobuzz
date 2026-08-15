3007ec2c9e95e5b0d2d82d0f2653b79b4a7ef14e
BiggerBozo <cflibftc@gmail.com>
2026-08-15 13:08:51 -0700

    ka done (could also be 3.5 prob)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -68,7 +68,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 1.1303356764959522;
         public double kV = 0.00023942297840956245;
-        public double kA = 0;
+        public double kA = 0.00004;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
