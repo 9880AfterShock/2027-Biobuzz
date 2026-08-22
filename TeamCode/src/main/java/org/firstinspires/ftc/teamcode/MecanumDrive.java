c9daca12dedaa16ecfa3a407060b8472ee989c28
BiggerBozo <cflibftc@gmail.com>
2026-08-22 13:05:39 -0700

    adgusting logger for max speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -62,7 +62,7 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.001978956; //32*0.03937008*Math.PI/2000; //diameter of the wheel in mm (32), to inches, to circumference (PI), divided by CPR (2000)
-        public double lateralInPerTick = 0.001494276006481848;
+        public double lateralInPerTick = 0.0014573709729097952;
         public double trackWidthTicks = 6819.473277240446;
 
         // feedforward parameters (in tick units)
