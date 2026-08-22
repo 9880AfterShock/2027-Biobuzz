197da5ddffdf69efd4b3a5d2a9742f2bae7badc1
BiggerBozo <cflibftc@gmail.com>
2026-08-22 13:01:26 -0700

    using pinpint values for inpertick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -61,7 +61,7 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
         // drive model parameters
-        public double inPerTick = 0.00197896; //32*0.03937008*Math.PI/2000; //diameter of the wheel in mm (32), to inches, to circumference (PI), divided by CPR (2000)
+        public double inPerTick = 0.001978956; //32*0.03937008*Math.PI/2000; //diameter of the wheel in mm (32), to inches, to circumference (PI), divided by CPR (2000)
         public double lateralInPerTick = 0.001494276006481848;
         public double trackWidthTicks = 6819.473277240446;
 
